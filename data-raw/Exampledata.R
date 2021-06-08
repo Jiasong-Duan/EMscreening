02efbff3b0ed352876d6bf1e921ada1cbfdd17b6
Jiasong Duan <duan.jiasong@yahoo.com>
2021-06-07 22:56:36 -0500

    Updated example

--- data-raw/Exampledata.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `Exampledata` dataset goes here
-
-load("D:/Building R package/EMscreening/data-raw/PhenotypeE.RData")
-PhenotypeExample= Phenotype
-load("D:/Building R package/EMscreening/data-raw/DNAmE.RData")
-DNAmExample = DNAm
-
-usethis::use_data(PhenotypeExample, DNAmExample, overwrite = TRUE)
